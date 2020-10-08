8a208eba7779891f9d2eca6e62be7b339479ac25
Ido Shamay <idoshamay@gmail.com>
2020-10-08 13:03:44 +0300

    Base configuration for quota preemotion testing and verification

--- condor_cluster/sh_loop.cmd
+++ condor_cluster/sh_loop.cmd
@@ -9,6 +9,8 @@ output		= sh_loop_$(Process).out
 error		= sh_loop_$(Process).err
 log		    = sh_loop.log
 arguments	= 600
+accounting_group = A.a
+accounting_group_user = amit
 should_transfer_files   = IF_NEEDED
 when_to_transfer_output = ON_EXIT
 queue 30
